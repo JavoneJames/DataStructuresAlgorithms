4b94ec092ba28a1dfeddc98b34a05b6257fbf39f
jovane james <jovanekjames@icloud.com>
2019-09-11 15:46:53 +0100

    updated comments

--- linkedlist/src/LinkedList.java
+++ linkedlist/src/LinkedList.java
@@ -2,7 +2,7 @@ public class LinkedList {
 
     private Node front;//object of the Node class
 
-    public LinkedList()
+    private LinkedList()
     {
         front = null;//initialize the object to be null
     }
@@ -17,11 +17,18 @@ public class LinkedList {
         if (front == null){//checks if the front is null - if so creates a new object that adds to the front
             front = new Node(i, null);
         }else{
-            Node cell = front;//creates object of the current linkedlist
+            Node cell = front;//creates a copy object of the current linkedlist
             while (cell.next != null)//loops through the list whilst there items
                 cell = cell.next;
-            cell.next = new Node(i, null);
+            cell.next = new Node(i, null);//creates a new object that adds to the back
         }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addToFront(1);
+        ll.addToBack(2);
+        ll.addToBack(3);
     }
 
 
