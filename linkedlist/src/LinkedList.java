0e88490b17b6be32959e35dec34b3b9c9bd10bb6
jovane james <jovanekjames@icloud.com>
2019-09-11 10:47:10 +0100

    methods that adds to the back of the linkedlist

--- linkedlist/src/LinkedList.java
+++ linkedlist/src/LinkedList.java
@@ -1,14 +1,28 @@
 public class LinkedList {
 
-    private Node front;
+    private Node front;//object of the Node class
 
     public LinkedList()
     {
-        front = null;
+        front = null;//initialize the object to be null
     }
 
     private void addToFront(int i)
     {
-        front = new Node(i, front);
+        front = new Node(i, front);//creates a new object that adds to the front
     }
+
+    private void addToBack(int i)
+    {
+        if (front == null){//checks if the front is null - if so creates a new object that adds to the front
+            front = new Node(i, null);
+        }else{
+            Node cell = front;//creates object of the current linkedlist
+            while (cell.next != null)//loops through the list whilst there items
+                cell = cell.next;
+            cell.next = new Node(i, null);
+        }
+    }
+
+    
 }
