ef2d4564720386e487f4a00cca1e5ff1541485be
jovane james <jovanekjames@icloud.com>
2019-09-13 22:59:03 +0100

    added a custom class exception that extends RunTimeException

--- linkedlist/src/Node.java
+++ linkedlist/src/Node.java
@@ -1,10 +1,17 @@
 class Node {
 
-    private int data;//store data to be added to the list
+    int data;//store data to be added to the list
     Node next;//a reference to the next object
 
     Node(int data, Node next) {
         this.data = data;
         this.next = next;
+    }
+}
+
+class ListException extends RuntimeException{
+
+    public ListException(String s){
+        super(s);
     }
 }
