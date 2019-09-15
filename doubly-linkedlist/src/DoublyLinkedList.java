114de9bbab81755a72900026e72db29ac40e66ac
jovane james <jovanekjames@icloud.com>
2019-09-15 01:13:09 +0100

    added a function addToBack

--- doubly-linkedlist/src/DoublyLinkedList.java
+++ doubly-linkedlist/src/DoublyLinkedList.java
@@ -2,7 +2,7 @@ public class DoublyLinkedList {
 
     private Node front;
 
-    DoublyLinkedList(){
+    private DoublyLinkedList(){
         front = null;
     }
 
@@ -11,5 +11,17 @@ public class DoublyLinkedList {
         front = new Node(i, front, null);
     }
 
+    private void addToBack(int i)
+    {
+        if (front == null)//checks if the front is null
+          front = new Node(i, front, null);//creates a new object and adds to the front
+        else{
+            Node cell = front;
+            while (cell.next != null)//loops through list
+                cell = cell.next;//change to the next node
+            cell.next = new Node(i, null, cell);//create a new object, adds element,
+            // next is null and prev points to prev head
+        }
+    }
 
 }
