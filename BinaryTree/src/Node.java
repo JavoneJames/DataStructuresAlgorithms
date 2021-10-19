e22bbdd81cfd83f53114ab087ea42745f897d24e
Jovane <jovanekjames@icloud.com>
2021-10-19 19:40:38 +0100

    added code for binary tree

---
+++ BinaryTree/src/Node.java
@@ -0,0 +1,15 @@
+public class Node<T>{
+    T data;
+    Node<T> left;
+    Node<T> right;
+    Node(T i){
+        data = i;
+        left = null;
+        right = null;
+    }
+    Node(T i, Node<T> left, Node<T> right){
+        data = i;
+        this.left = left;
+        this.right = right;
+    }
+}
